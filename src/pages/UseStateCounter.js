a0083956adffcafb187659726d9919e3a6d4cb7b
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-08-06 18:41:43 +0530

    feat: add useState counter

--- src/pages/UseStateCounter.js
+++ src/pages/UseStateCounter.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function UseStateCounter() {
+const [count , setCount] = useState(0);
+const increment = ()=>{
+  console.log('count', count)
+  setCount(count + 1);
+}
+
+const decrement = ()=>{
+  console.log('count', count)
+  setCount(count - 1);
+}
   return (
     <div className="App">
-      <h1>USC</h1>
+      <h1>{`Count: ${count}`}</h1>
+      <button className="action-btn" onClick={increment}>+</button>
+      <button className="action-btn" onClick={decrement}>-</button>
     </div>
   );
 }
